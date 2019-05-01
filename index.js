15398081c3cdd1e738f84e2c44fc5393ba602b4b
dskrenta <dskrenta@gmail.com>
2019-04-30 18:01:50 -0700

    updates

--- index.js
+++ index.js
@@ -1,43 +1,50 @@
 (() => {
   'use strict';
 
+  const eyegaze = false;
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
   const canvasBoundingRect = canvas.getBoundingClientRect();
   const frameLength = 2;
   let mouseX = 0;
   let mouseY = 0;
+  let score = 0;
 
   const circles = [
-    { x: 100, y: 100, radius: 25, xMove: '+', yMove: '+' },
-    { x: 40, y: 200, radius: 25, xMove: '-', yMove: '+' },
-    { x: 250, y: 300, radius: 25, xMove: '+', yMove: '-' },
-    { x: 150, y: 35, radius: 25, xMove: '-', yMove: '-' }
+    { x: 100, y: 100, radius: 10, xMove: '+', yMove: '+', color: 'red' },
+    { x: 40, y: 200, radius: 15, xMove: '-', yMove: '+', color: 'blue' },
+    { x: 250, y: 300, radius: 30, xMove: '+', yMove: '-', color: 'red' },
+    { x: 150, y: 35, radius: 40, xMove: '-', yMove: '-', color: 'blue' },
+    { x: 134, y: 523, radius: 19, xMove: '+', yMove: '+', color: 'red' },
+    { x: 40, y: 222, radius: 53, xMove: '-', yMove: '+', color: 'blue' },
+    { x: 234, y: 532, radius: 10, xMove: '+', yMove: '-', color: 'red' },
+    { x: 342, y: 123, radius: 5, xMove: '-', yMove: '-', color: 'blue' }
   ];
 
   const rect = {
-    x: 100,
-    y: 100,
-    width: 25,
-    height: 25
+    x: 300,
+    y: 300,
+    width: 50,
+    height: 50
   };
 
   canvasInit();
 
   function canvasInit() {
-    // set width and height of the canvas to the window dimensions
+    // Set width and height of the canvas to the window dimensions
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    // draw circles
+    // Draw rect
+    drawRect(rect);
+
+    // Draw circles
     for (let i = 0; i < circles.length; i++) {
       drawCircle(circles[i]);
     };
 
-    // set animation timeout
-    setTimeout(() => {
-      window.requestAnimationFrame(moveCircle);
-    }, 2500);
+    // First animation frame
+    window.requestAnimationFrame(moveCircle);
   }
 
   function clearCanvas() {
@@ -47,35 +54,48 @@
   function moveCircle() {
     clearCanvas();
 
-    // Iterate over all the circles.
-    for(let i = 0; i < circles.length; i++) {
+    // Draw rect
+    drawRect({
+      ...rect,
+      x: mouseX,
+      y: mouseY
+    });
 
-      if( circles[i].xMove == '+' ) {
+    // Iterate over all the circles.
+    for (let i = 0; i < circles.length; i++) {
+      const collision = intersects(circles[i], rect);
+      if(collision) score += 1;
+
+      if (circles[i].xMove == '+') {
         circles[i].x += frameLength;
-      } else {
+      }
+      else {
         circles[i].x -= frameLength;
       }
-      if( circles[i].yMove == '+' ) {
+      if (circles[i].yMove == '+') {
         circles[i].y += frameLength;
-      } else {
+      }
+      else {
         circles[i].y -= frameLength;
       }
 
       drawCircle(circles[i])
 
-      if( (circles[i].x + circles[i].radius) >= canvas.width ) {
+      if((circles[i].x + circles[i].radius) >= canvas.width) {
         circles[i].xMove = '-';
       }
-      if( (circles[i].x - circles[i].radius) <= 0 ) {
+      if((circles[i].x - circles[i].radius) <= 0) {
         circles[i].xMove = '+';
       }
-      if( (circles[i].y + circles[i].radius) >= canvas.height ) {
+      if((circles[i].y + circles[i].radius) >= canvas.height) {
         circles[i].yMove = '-';
       }
-      if( (circles[i].y - circles[i].radius) <= 0 ) {
+      if((circles[i].y - circles[i].radius) <= 0) {
         circles[i].yMove = '+';
       }
     }
+
+    displayScore(score);
 
     // Render it again
     window.requestAnimationFrame(moveCircle);
@@ -94,38 +114,41 @@
   function drawCircle({
     x,
     y,
-    radius
+    radius,
+    color
   }) {
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = '#F03C69';
+    ctx.fillStyle = color;
     ctx.fill();
   }
 
-  /*
-  canvas.addEventListener('mousemove', e => {
-    mouseX = e.clientX - canvasBoundingRect.left;
-    mouseY = e.clientY - canvasBoundingRect.top;
-    console.log(mouseX, mouseY);
-    clearCanvas();
-    drawRect({
-      x: mouseX,
-      y: mouseY,
-      width: 25,
-      height: 25
+  function intersects(circle, rect) {
+    const DeltaX = circle.x - Math.max(rect.x, Math.min(circle.x, rect.x + rect.width));
+    const DeltaY = circle.y - Math.max(rect.y, Math.min(circle.y, rect.y + rect.height));
+    return (DeltaX * DeltaX + DeltaY * DeltaY) < (circle.radius * circle.radius);
+  }
+
+  function displayScore(text, x = 300, y = 300, fontSize = '50') {
+    ctx.font = `${fontSize}px Helvetica`;
+    ctx.fillText(`Score: ${text}`, x, y);
+  }
+
+  if (eyegaze) {
+    webgazer.setGazeListener(function(data, elapsedTime) {
+      if (data == null) {
+        return;
+      }
+
+      var xprediction = data.x; //these x coordinates are relative to the viewport
+      var yprediction = data.y; //these y coordinates are relative to the viewport
+      console.log(xprediction, yprediction, elapsedTime); //elapsed time is based on time since begin was called
+    }).begin();
+  }
+  else {
+    canvas.addEventListener('mousemove', e => {
+      mouseX = e.clientX - canvasBoundingRect.left;
+      mouseY = e.clientY - canvasBoundingRect.top;
     });
-  });
-  */
-
-  /*
-  webgazer.setGazeListener(function(data, elapsedTime) {
-    if (data == null) {
-      return;
-    }
-
-    var xprediction = data.x; //these x coordinates are relative to the viewport
-    var yprediction = data.y; //these y coordinates are relative to the viewport
-    console.log(xprediction, yprediction, elapsedTime); //elapsed time is based on time since begin was called
-  }).begin();
-  */
+  }
 })();
